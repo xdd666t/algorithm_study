ec121305655f103a9351243f1d4da8200ca70f2a
小呆呆666 <xdd666t@gmail.com>
2021-11-15 22:50:17 +0800

    two day

---
+++ src/com/study/two/two_sum/Solution.java
@@ -0,0 +1,19 @@
+package com.study.two.two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+//链接：https://leetcode-cn.com/problems/two-sum/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
