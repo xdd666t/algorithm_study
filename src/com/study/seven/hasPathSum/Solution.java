c0064726b6b4e61a311304e0fa22f73f3dcdb36c
小呆呆666 <xdd666t@gmail.com>
2021-11-28 23:00:55 +0800

    commit

--- src/com/study/seven/hasPathSum/Solution.java
+++ src/com/study/seven/hasPathSum/Solution.java
@@ -6,28 +6,59 @@ package com.study.seven.hasPathSum;
 //
 //链接：https://leetcode-cn.com/problems/path-sum
 
-//class Solution {
-//    public boolean hasPathSum(TreeNode root, int targetSum) {
-//
-//    }
-//}
-//
-//
-//class TreeNode {
-//    int val;
-//    TreeNode left;
-//    TreeNode right;
-//
-//    TreeNode() {
-//    }
-//
-//    TreeNode(int val) {
-//        this.val = val;
-//    }
-//
-//    TreeNode(int val, TreeNode left, TreeNode right) {
-//        this.val = val;
-//        this.left = left;
-//        this.right = right;
-//    }
-//}
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> numQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        numQueue.offer(root.val);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            int num = numQueue.poll();
+            if (node.left == null && node.right == null) {
+                if (num == targetSum) {
+                    return true;
+                }
+            }
+
+            if (node.left != null) {
+                nodeQueue.offer(node.left);
+                numQueue.offer(node.left.val + num);
+            }
+
+            if (node.right != null) {
+                nodeQueue.offer(node.right);
+                numQueue.offer(node.right.val + num);
+            }
+        }
+
+        return false;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
