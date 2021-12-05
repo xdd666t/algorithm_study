c6a61cb1c257864197d6151a467c9eed561336b2
小呆呆666 <xdd666t@gmail.com>
2021-12-05 22:59:00 +0800

    commit

--- src/com/study/nine/fib/Solution.java
+++ src/com/study/nine/fib/Solution.java
@@ -11,17 +11,17 @@ package com.study.nine.fib;
 //链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
 class Solution {
     public int fib(int n) {
-        final int mod = 1000000007;
         if (n < 2) {
             return n;
         }
-        int a = 0, b = 0, s = 1;
+        int a = 0, b = 1, sum = 0;
         for (int i = 2; i <= n; i++) {
+            sum = (a + b) % 1000000007;
             a = b;
-            b = s;
-            s = (a + b) % mod;
+            b = sum;
+
         }
 
-        return s;
+        return sum;
     }
 }
