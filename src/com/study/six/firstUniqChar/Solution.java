ddeda94d3bcac922e87e9de81a7d45f63c59da6f
XDD666 <xdd666@foxmail.com>
2021-12-15 20:45:48 +0800

    commit

---
+++ src/com/study/six/firstUniqChar/Solution.java
@@ -0,0 +1,40 @@
+package com.study.six.firstUniqChar;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+//50. 第一个只出现一次的字符
+//在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+//
+//示例 1:
+//
+//输入：s = "abaccdeff"
+//输出：'b'
+//示例 2:
+//
+//输入：s = ""
+//输出：' '
+//
+//链接：https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/solution/
+class Solution {
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!map.containsKey(ch)) {
+                map.put(ch, i);
+                queue.offer(ch);
+            } else {
+                map.put(ch, -1);
+                while (!queue.isEmpty() && map.get(queue.peek()) == -1) {
+                    queue.poll();
+                }
+            }
+        }
+
+        return queue.isEmpty() ? ' ' : queue.poll();
+    }
+}
--- src/com/study/six/getLeastNumbers/Solution.java
+++ src/com/study/six/getLeastNumbers/Solution.java
@@ -1,9 +1,8 @@
 package com.study.six.getLeastNumbers;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 //40. 最小的k个数
 //输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
@@ -18,36 +17,27 @@ import java.util.PriorityQueue;
 //输出：[0]
 //
 //链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
-//class Solution {
-//    public int[] getLeastNumbers(int[] arr, int k) {
-//        Arrays.sort(arr);
-//        return Arrays.copyOfRange(arr, 0, k);
-//    }
-//}
-
 class Solution {
     public int[] getLeastNumbers(int[] arr, int k) {
-        int[] vec = new int[k];
-        if (k == 0) { // 排除 0 的情况
-            return vec;
-        }
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
-            public int compare(Integer num1, Integer num2) {
-                return num2 - num1;
+        int[] result = new int[k];
+        if (k == 0) return result;
+
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
             }
         });
-        for (int i = 0; i < k; ++i) {
-            queue.offer(arr[i]);
-        }
-        for (int i = k; i < arr.length; ++i) {
+
+        for (int i = 0; i < k; i++) queue.offer(arr[i]);
+        for (int i = k; i < arr.length; i++) {
             if (queue.peek() > arr[i]) {
                 queue.poll();
                 queue.offer(arr[i]);
             }
         }
-        for (int i = 0; i < k; ++i) {
-            vec[i] = queue.poll();
-        }
-        return vec;
+        for (int i = 0; i < k; i++) result[i] = queue.poll();
+
+        return result;
     }
 }
