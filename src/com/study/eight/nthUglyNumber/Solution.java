389c83df0268c00f52cd494e6834689b6c3baed3
XDD666 <xdd666@foxmail.com>
2021-12-17 18:00:15 +0800

    commit

---
+++ src/com/study/eight/nthUglyNumber/Solution.java
@@ -0,0 +1,39 @@
+package com.study.eight.nthUglyNumber;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+//49. 丑数
+//我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+//
+//输入: n = 10
+//输出: 12
+//解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+//
+//说明:
+//1 是丑数。
+//n 不超过1690。
+//
+//链接：https://leetcode-cn.com/problems/chou-shu-lcof/
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
