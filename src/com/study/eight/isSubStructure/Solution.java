181c28354a20bad2f241f9cc5c02b715208db360
小呆呆666 <xdd666t@gmail.com>
2021-12-19 22:48:47 +0800

    commit

--- src/com/study/eight/isSubStructure/Solution.java
+++ src/com/study/eight/isSubStructure/Solution.java
@@ -28,7 +28,13 @@ package com.study.eight.isSubStructure;
 //链接：https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
 class Solution {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
 
+    private boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
     }
 }
 
