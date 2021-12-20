cf4ba224408c3846156ece1f523c59e612a1452c
XDD666 <xdd666@foxmail.com>
2021-12-20 16:49:45 +0800

    Update Solution.java

--- src/com/study/nine/permutation/Solution.java
+++ src/com/study/nine/permutation/Solution.java
@@ -1,5 +1,8 @@
 package com.study.nine.permutation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //38. 字符串的排列
 //输入一个字符串，打印出该字符串中字符的所有排列。
 //
@@ -9,5 +12,28 @@ package com.study.nine.permutation;
 //输出：["abc","acb","bac","bca","cab","cba"]
 //
 //链接：https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/
-public class Solution {
+class Solution {
+    private Set<String> result = new HashSet<>();
+
+    public String[] permutation(String s) {
+        search(s.toCharArray(), new StringBuilder(), new boolean[s.length()]);
+        return result.toArray(new String[0]);
+    }
+
+    void search(char[] ch, StringBuilder sb, boolean[] visited) {
+        if (sb.length() == ch.length) {
+            result.add(sb.toString());
+        }
+
+        for (int i = 0; i < ch.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            sb.append(ch[i]);
+
+            search(ch, sb, visited);
+
+            sb.deleteCharAt(sb.length() - 1);
+            visited[i] = false;
+        }
+    }
 }
