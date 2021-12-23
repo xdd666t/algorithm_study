acc86e5612b948280f7fc4e27dd701118bd5bada
XDD666 <xdd666@foxmail.com>
2021-12-23 16:08:10 +0800

    commit

--- src/com/study/ten/dicesProbability/Solution.java
+++ src/com/study/ten/dicesProbability/Solution.java
@@ -11,9 +11,29 @@ package com.study.ten.dicesProbability;
 //输入: 2
 //输出: [0.02778,0.05556,0.08333,0.11111,0.13889,0.16667,0.13889,0.11111,0.08333,0.05556,0.02778]
 //
+//限制：
+//1 <= n <= 11
+//
 //链接：https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof/
 class Solution {
     public double[] dicesProbability(int n) {
+        int[][] dp = new int[n + 1][6 * n + 1];
+        for (int i = 1; i <= 6; i++) {
+            dp[1][i] = 1;
+        }
 
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= 6 * n; j++) {
+                for (int k = 1; k <= 6 && j > k; k++) {
+                    dp[i][j] += dp[i - 1][j - k];
+                }
+            }
+        }
+
+        double[] result = new double[5 * n + 1];
+        for (int i = n; i <= 6 * n; i++) {
+            result[i - n] = (double) dp[n][i] / Math.pow(6, n);
+        }
+        return result;
     }
 }
