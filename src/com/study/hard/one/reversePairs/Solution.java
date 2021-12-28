99aef967fe879439f5abc2f79d64f33037bf85ca
XDD666 <xdd666@foxmail.com>
2021-12-28 10:38:40 +0800

    Update Solution.java

--- src/com/study/hard/one/reversePairs/Solution.java
+++ src/com/study/hard/one/reversePairs/Solution.java
@@ -10,7 +10,45 @@ package com.study.hard.one.reversePairs;
 //
 //链接：https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/
 class Solution {
-    public int reversePairs(int[] nums) {
+    int counts;
 
+    public int reversePairs(int[] nums) {
+        counts = 0;
+        merge(nums, 0, nums.length - 1);
+        return counts;
+    }
+
+    private void merge(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            merge(nums, left, mid);
+            merge(nums, mid + 1, right);
+            mergeSort(nums, left, mid, right);
+        }
+    }
+
+    private void mergeSort(int[] nums, int left, int mid, int right) {
+        int[] tempArray = new int[right - left + 1];
+        int index = 0;
+        int low = left, high = mid + 1;
+        while (low <= mid && high <= right) {
+            if (nums[low] <= nums[high]) {
+                tempArray[index++] = nums[low++];
+            } else {
+                tempArray[index++] = nums[high++];
+                counts += mid - low + 1;
+            }
+        }
+
+        while (low <= mid) {
+            tempArray[index++] = nums[low++];
+        }
+        while (high <= right) {
+            tempArray[index++] = nums[high++];
+        }
+
+        for (int i = 0; i < tempArray.length; i++) {
+            nums[left + i] = tempArray[i];
+        }
     }
 }
