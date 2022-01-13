c4ef4b3ac773cc15d878e064fec43e88855cd80a
小呆呆666 <xdd666t@gmail.com>
2022-01-13 22:59:38 +0800

    Update Solution.java

--- src/com/study/medium/two/cuttingRope1/Solution.java
+++ src/com/study/medium/two/cuttingRope1/Solution.java
@@ -23,7 +23,8 @@ class Solution {
         dp[2] = 1;
         for (int i = 3; i < n + 1; i++) {
             for (int j = 2; j < i; j++) {
-                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+                int cur = Math.max(j * (i - j), j * dp[i - j]);
+                dp[i] = Math.max(dp[i], cur);
             }
         }
         return dp[n];
