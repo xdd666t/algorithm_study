a39bef34ff5197d347170cc022fc62fe765e86d5
小呆呆666 <xdd666t@gmail.com>
2022-03-14 23:07:31 +0800

    commit

--- src/com/study/easy/one/max_sub_array/Solution.java
+++ src/com/study/easy/one/max_sub_array/Solution.java
@@ -17,19 +17,11 @@ package com.study.easy.one.max_sub_array;
 //链接：https://leetcode-cn.com/problems/maximum-subarray/
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int result = nums[0];
-        int sum = 0;
+        int temp = 0, max = nums[0];
         for (int num : nums) {
-            sum += num;
-            result = Math.max(result, sum);
-
-            if (sum < 0) {
-                sum = 0;
-            }
+            temp = Math.max(temp + num, num);
+            max = Math.max(temp, max);
         }
-        return result;
+        return max;
     }
 }
